f70d1ec9f3c237526a4f43c372742c73d32c6fe0
hossein <hossein@hosseins-MacBook-Pro.local>
2018-01-09 19:47:38 +0330

    ice problem

--- js/main.js
+++ js/main.js
@@ -167,7 +167,7 @@ function handleIceCandidate(event) {
       type: 'candidate',
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate});
+      candidate: event.candidate});
   } else {
     console.log('End of candidates.');
   }
