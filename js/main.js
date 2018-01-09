eb33b837975b9ee249db92317abc6b3416b77cd1
hossein <hossein@hosseins-MacBook-Pro.local>
2018-01-09 16:41:34 +0330

    ice problem

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+
+
 'use strict';
 
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition
+  || window.msSpeechRecognition || window.oSpeechRecognition;
+  
 var isChannelReady;
 var isInitiator = false;
 var isStarted = false;
